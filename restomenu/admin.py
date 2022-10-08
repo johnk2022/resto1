3763b0c910c7ed7fe69a91f8a069bc688a6f15a1
johnk2022 <keenan.john@hotmail.com>
2022-10-08 13:08:23 +0000

    Fix minor typo in settings.py and add commands.txt

--- restomenu/admin.py
+++ restomenu/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Menu)
-class PostAdmin(SummernoteModelAdmin):
+class MenuAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
@@ -13,12 +13,12 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
-#     list_filter = ('approved', 'created_on')
-#     search_fields = ('name', 'email', 'body')
-#     actions = ['approve_comments']
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'menu', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
-#     def approve_comments(self, request, queryset):
-#         queryset.update(approved=True)
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
